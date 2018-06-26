4310b8d941f3de208bdd45cefed330459aecc2a2
Benjamin Bradnick <ben.bradnick@diamond.ac.uk>
2018-06-26 15:45:01 +0100

    Modified andordriverpart to set trigger mode to External

--- malcolm/modules/ADAndor/parts/andordriverpart.py
+++ malcolm/modules/ADAndor/parts/andordriverpart.py
@@ -5,6 +5,8 @@ class AndorDriverPart(ExposureDetectorDriverPart):
     def setup_detector(self, child, completed_steps, steps_to_do, params=None):
         fs = super(AndorDriverPart, self).setup_detector(
             child, completed_steps, steps_to_do, params)
+        # Set Andor trigger mode
+        fs.append(child.triggerMode.put_value_async("External"))
         child.wait_all_futures(fs)
         # Need to reset acquirePeriod as it's sometimes wrong
         fs = child.acquirePeriod.put_value_async(
